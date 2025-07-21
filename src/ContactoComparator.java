75ea9d90b24ed6ae1e01bd9b0b644e1a29d04a09
JesusFranciscoCruz <cruz.ramirez.jesus.francisco@gmail.com>
2025-07-21 15:47:43 -0600

    Archivos nuevos

--- src/ContactoComparator.java
+++ src/ContactoComparator.java
@@ -3,6 +3,7 @@ package src;
 import java.util.Comparator;
 
 public class ContactoComparator implements Comparator<Contacto>{
+   
     @Override
     public int compare(Contacto contacto1, Contacto contacto2) {
         int firstNameComparison = contacto1.getFirstName().compareTo(contacto2.getFirstName());
