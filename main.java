c3ae516fe34943896f996cb82f3ba4b4dbed109a
Alam Dominic <alamdominic16@gmail.com>
2025-07-21 10:40:47 -0600

    Se sube metodo main

--- main.java
+++ main.java
@@ -1,20 +1,31 @@
 public class main {
-    private String Name;
+    private String FirstName;
+    private String SecondName;
     private Long Number;
 
     // Constructor
-    public main(String Name, Long Number) {
-        this.Name = Name;
+    public main(String FirstName, String SecondName, Long Number) {
+        this.FirstName = FirstName;
+        this.SecondName = SecondName;
         this.Number = Number;
     }
 
-    // Getter y Setter para Name
-    public String getName() {
-        return Name;
+    // Getter y Setter para FirstName
+    public String getFirstName() {
+        return FirstName;
     }
 
-    public void setName(String Name) {
-        this.Name = Name;
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    // Getter y Setter para SecondName
+    public String getSecondName() {
+        return SecondName;
+    }
+
+    public void setSecondName(String SecondName) {
+        this.SecondName = SecondName;
     }
 
     // Getter y Setter para Number
@@ -29,7 +40,8 @@ public class main {
     @Override
     public String toString() {
         return "main{" +
-                "Name='" + Name + '\'' +
+                "FirstName='" + FirstName + '\'' +
+                ", SecondName='" + SecondName + '\'' +
                 ", Number=" + Number +
                 '}';
     }
